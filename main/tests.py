affcd9ad4fbb7439c487ed97c39c2053dfac5077
barbarian47 <barbarians47@outloock.com>
2022-12-25 09:27:42 +0300

    add tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,11 @@
-# from django.test import TestCase
+from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestSomething(TestCase):
+    def test_dummy(self):
+        self.assertEqual(5*10, 50)
+    
+    def test_dummy_2(self):
+        self.assertIn(5, [1, 2, 3, 4, 5])
