85a4fe5652a6d137f4a90ef875cb6a2506d8eb44
barbarian47 <barbarians47@outloock.com>
2022-12-25 09:33:49 +0300

    Fix tests

--- main/tests.py
+++ main/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 
+
 # Create your tests here.
 
-
 class TestSomething(TestCase):
+
     def test_dummy(self):
         self.assertEqual(5*10, 50)
-    
+
     def test_dummy_2(self):
         self.assertIn(5, [1, 2, 3, 4, 5])
